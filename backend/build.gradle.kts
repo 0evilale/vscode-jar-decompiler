58d56fb487fdf604da151222bee3368a2b8004ea
Erick <evilale@outlook.com>
2026-03-14 16:17:19 -0600

    build: scaffold backend Gradle project with CFR + Vineflower + Gson

---
+++ backend/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "decompiler"
+version = "1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.fabricmc.net") }
+}
+
+dependencies {
+    implementation("net.fabricmc:cfr:0.2.2")
+    implementation("org.vineflower:vineflower:1.11.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+tasks.shadowJar {
+    archiveFileName = "decompiler-backend.jar"
+    archiveClassifier = null
+    manifest {
+        attributes["Main-Class"] = "decompiler.cli.CliMain"
+    }
+}
+
+tasks.jar { enabled = false }
